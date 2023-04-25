5b09a27686f8c672156aeea34156884df2f25ef0
Kautilya Save <kautilyasave@gmail.com>
2023-04-25 14:23:31 -0600

    basicAVPlayer framework created

---
+++ BasicAVPlayer/BasicAVPlayer.h
@@ -0,0 +1,18 @@
+//
+//  BasicAVPlayer.h
+//  BasicAVPlayer
+//
+//  Created by Kautilya Save on 4/25/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BasicAVPlayer.
+FOUNDATION_EXPORT double BasicAVPlayerVersionNumber;
+
+//! Project version string for BasicAVPlayer.
+FOUNDATION_EXPORT const unsigned char BasicAVPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BasicAVPlayer/PublicHeader.h>
+
+
